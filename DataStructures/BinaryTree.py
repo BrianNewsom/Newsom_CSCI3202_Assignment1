524d0fbf63f64d43120dfa58260daf39f748a7d3
Brian Newsom <brian.newsom@colorado.edu>
2015-08-24 23:15:43 -0600

    Finish binary tree functions, no tests yet

--- DataStructures/BinaryTree.py
+++ DataStructures/BinaryTree.py
@@ -1,4 +1,6 @@
 from Node import Node
+# So we can call out BinaryTree print function 'print'
+# from __future__ import print_function
 
 class BinaryTree:
 	root = None
@@ -31,3 +33,45 @@ class BinaryTree:
 			if not self.add_recursive(self.root, value, parent_value):
 				print "Parent not found"
 
+	def delete(self, value):
+		print "Attempting to delete %s" % value
+		if not self.delete_recursive(self.root, value):
+			print "Node not found"
+
+	def delete_recursive(self, head, value):
+		if head is None:
+			return False
+		
+		if head.key is value:
+			if head.left is not None or head.right is not None:
+				print "Node not deleted, has children"
+			else:
+				parent = head.parent
+				if not parent:
+					# if no parent, we don't need to sever any links, only change root
+					self.root = None
+				else:
+					# Otherwise we'll need to delete the ref to the node
+					if parent.left and parent.left.key is value:
+						parent.left = None
+					elif parent.right and parent.right.key is value:
+						parent.right = None
+				
+			return True
+		else:
+			return self.delete_recursive(head.left, value) or self.delete_recursive(head.right, value)
+
+	def print_tree(self):
+		# Can't override python print function, must rename to print_tree
+		print "Printing Binary Tree:"
+		if self.root:
+			self.print_rec(self.root)
+
+	def print_rec(self, head):
+		# Perform in order traversal
+		print head.key
+		if head.left:
+			self.print_rec(head.left)
+		if head.right:
+			self.print_rec(head.right)
+		
--- Newsom_Assignment1.py
+++ Newsom_Assignment1.py
@@ -10,3 +10,15 @@ if __name__ == "__main__":
 	bt.add(4,2)
 	bt.add(5,2)
 	bt.add(6,4)
+	bt.print_tree()
+
+	bt.delete(6)
+	bt.print_tree()
+	
+	bt.delete(3)
+	bt.print_tree()
+	bt.delete(4)
+	bt.print_tree()
+	
+	bt.delete(2)
+	bt.print_tree()
